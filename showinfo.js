94b040ae9ed91df04bf891c0d86ca7ab44bc905a
claralacour <denlacour@gmail.com>
2023-09-19 10:48:49 +0200

    yay

---
+++ showinfo.js
@@ -0,0 +1,8 @@
+function showInfo(){
+    let boxInformation = document.getElementById("esbjerg");
+    if (boxInformation.style.display === "block"){
+        boxInformation.style.display = "none";
+    } else {
+        boxInformation.style.display = "block";
+    }
+}
